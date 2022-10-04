7e132a9b1364f989ed5baf9ebc609868605014f8
alineyhiga <alineyhiga@gmail.com>
2022-10-04 18:20:02 -0300

    header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,9 @@
+import { ReservasComponent } from './components/reservas/reservas.component';
+import { LoginComponent } from './components/login/login.component';
+import { LocadoraComponent } from './components/locadora/locadora.component';
+import { HomeComponent } from './components/home/home.component';
+import { CarrosComponent } from './components/carros/carros.component';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
