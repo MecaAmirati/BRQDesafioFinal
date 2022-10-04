628812f14de381a57cd0c1493723e492c3691900
Amirati <matheusamirati@gmail.com>
2022-10-04 14:20:13 -0300

    componentes criados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
+import { CarrosComponent } from './components/carros/carros.component';
+import { ReservasComponent } from './components/reservas/reservas.component';
+import { LocadoraComponent } from './components/locadora/locadora.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    LoginComponent,
+    CadastroComponent,
+    PerfilComponent,
+    CarrosComponent,
+    ReservasComponent,
+    LocadoraComponent
   ],
   imports: [
     BrowserModule,
