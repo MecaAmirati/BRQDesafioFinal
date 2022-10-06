8d27be162b3b5191c6530cc453d4849de4316b05
alineyhiga <alineyhiga@gmail.com>
2022-10-05 22:35:42 -0300

    validacao basica

--- src/app/components/locadora/locadora.component.ts
+++ src/app/components/locadora/locadora.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { UsuarioInterface } from 'src/app/model/usuario.model';
+import { UsuarioServiceService } from 'src/app/service/usuario-service/usuario.service.service';
 
 @Component({
   selector: 'app-locadora',
@@ -6,10 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./locadora.component.scss']
 })
 export class LocadoraComponent implements OnInit {
-
-  constructor() { }
+  formularioLocadora: FormGroup=this.formBuilder.group({
+    locadora: new FormControl('',[Validators.required]),
+    endereco: new FormControl('',[Validators.required]),
+    telefone:new FormControl('',[Validators.required, Validators.pattern("^[0-9]*$")])
+  });
+  constructor(
+    private formBuilder: FormBuilder,
+    private usuarioService: UsuarioServiceService
+    ) { }
 
   ngOnInit(): void {
+  }
+  Validacao(){
+    return `Este campo é obrigatório`
+  }
+  ValidacaoTelefone(){
+    if(this.formularioLocadora.controls['telefone'].hasError('required')){
+      return 'Este campo é obrigatório'
+    }
+    return  'Telefone inválido';
+  }
+  salvarDadosLocadora(){
+
   }
 
 }
